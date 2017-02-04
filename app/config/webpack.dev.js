de2b66310989ceced8df786def0791840ece1ed2
Alexander Shein <alexa-montana@live,com>
2017-02-05 01:04:31 +0300

    added webpack + angular2 app

---
+++ app/config/webpack.dev.js
@@ -0,0 +1,27 @@
+var webpackMerge = require('webpack-merge'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    commonConfig = require('./webpack.common.js'),
+    helpers = require('./helpers');
+
+module.exports = webpackMerge(commonConfig, {
+    devtool: 'cheap-module-eval-source-map',
+
+    output: {
+        path: helpers.root('dist'),
+        publicPath: 'http://localhost:8080/',
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js'
+    },
+
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'css/[name].css',
+            allChunks: true
+        })
+    ],
+
+    devServer: {
+        historyApiFallback: true,
+        stats: 'minimal'
+    }
+});
